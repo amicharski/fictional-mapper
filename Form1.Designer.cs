ec56b64769a7a6863b03544bb9dff07923878e08
Alex Micharski <amicharski@outlook.com>
2021-09-28 18:06:43 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace FictionalMapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.selectionNameBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.routeNumber = new System.Windows.Forms.TextBox();
+            this.shieldBoxToggle = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.shieldType = new System.Windows.Forms.ListBox();
+            this.drawRegionButton = new System.Windows.Forms.Button();
+            this.drawingRegions = new System.Windows.Forms.ListBox();
+            this.drawingLines = new System.Windows.Forms.ListBox();
+            this.toolState = new System.Windows.Forms.Label();
+            this.drawLineButton = new System.Windows.Forms.Button();
+            this.viewButton = new System.Windows.Forms.Button();
+            this.canvasPanel = new System.Windows.Forms.Panel();
+            this.topPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // topPanel
+            // 
+            this.topPanel.Controls.Add(this.selectionNameBox);
+            this.topPanel.Controls.Add(this.label2);
+            this.topPanel.Controls.Add(this.routeNumber);
+            this.topPanel.Controls.Add(this.shieldBoxToggle);
+            this.topPanel.Controls.Add(this.label1);
+            this.topPanel.Controls.Add(this.shieldType);
+            this.topPanel.Controls.Add(this.drawRegionButton);
+            this.topPanel.Controls.Add(this.drawingRegions);
+            this.topPanel.Controls.Add(this.drawingLines);
+            this.topPanel.Controls.Add(this.toolState);
+            this.topPanel.Controls.Add(this.drawLineButton);
+            this.topPanel.Controls.Add(this.viewButton);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(1272, 125);
+            this.topPanel.TabIndex = 0;
+            // 
+            // selectionNameBox
+            // 
+            this.selectionNameBox.Location = new System.Drawing.Point(706, 93);
+            this.selectionNameBox.Name = "selectionNameBox";
+            this.selectionNameBox.Size = new System.Drawing.Size(217, 22);
+            this.selectionNameBox.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(706, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Selection Name";
+            // 
+            // routeNumber
+            // 
+            this.routeNumber.Location = new System.Drawing.Point(706, 31);
+            this.routeNumber.Name = "routeNumber";
+            this.routeNumber.Size = new System.Drawing.Size(100, 22);
+            this.routeNumber.TabIndex = 9;
+            this.routeNumber.TextChanged += new System.EventHandler(this.routeNumber_TextChanged);
+            this.routeNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.routeNumber_KeyPress);
+            // 
+            // shieldBoxToggle
+            // 
+            this.shieldBoxToggle.AutoSize = true;
+            this.shieldBoxToggle.Location = new System.Drawing.Point(556, 32);
+            this.shieldBoxToggle.Name = "shieldBoxToggle";
+            this.shieldBoxToggle.Size = new System.Drawing.Size(18, 17);
+            this.shieldBoxToggle.TabIndex = 8;
+            this.shieldBoxToggle.UseVisualStyleBackColor = true;
+            this.shieldBoxToggle.CheckedChanged += new System.EventHandler(this.shieldBosToggle_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(577, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Highway";
+            // 
+            // shieldType
+            // 
+            this.shieldType.Enabled = false;
+            this.shieldType.FormattingEnabled = true;
+            this.shieldType.ItemHeight = 16;
+            this.shieldType.Items.AddRange(new object[] {
+            "County",
+            "State",
+            "U.S.",
+            "Interstate"});
+            this.shieldType.Location = new System.Drawing.Point(580, 31);
+            this.shieldType.Name = "shieldType";
+            this.shieldType.Size = new System.Drawing.Size(120, 84);
+            this.shieldType.TabIndex = 6;
+            // 
+            // drawRegionButton
+            // 
+            this.drawRegionButton.Location = new System.Drawing.Point(39, 70);
+            this.drawRegionButton.Name = "drawRegionButton";
+            this.drawRegionButton.Size = new System.Drawing.Size(168, 23);
+            this.drawRegionButton.TabIndex = 5;
+            this.drawRegionButton.Text = "Draw Region";
+            this.drawRegionButton.UseVisualStyleBackColor = true;
+            this.drawRegionButton.Click += new System.EventHandler(this.drawRegionButton_Click);
+            // 
+            // drawingRegions
+            // 
+            this.drawingRegions.FormattingEnabled = true;
+            this.drawingRegions.ItemHeight = 16;
+            this.drawingRegions.Items.AddRange(new object[] {
+            "Urban",
+            "Rural",
+            "Nature",
+            "Beach",
+            "Water"});
+            this.drawingRegions.Location = new System.Drawing.Point(339, 12);
+            this.drawingRegions.Name = "drawingRegions";
+            this.drawingRegions.Size = new System.Drawing.Size(120, 84);
+            this.drawingRegions.TabIndex = 4;
+            this.drawingRegions.SelectedIndexChanged += new System.EventHandler(this.drawingRegions_SelectedIndexChanged);
+            // 
+            // drawingLines
+            // 
+            this.drawingLines.FormattingEnabled = true;
+            this.drawingLines.ItemHeight = 16;
+            this.drawingLines.Items.AddRange(new object[] {
+            "Highway",
+            "Road",
+            "Creek"});
+            this.drawingLines.Location = new System.Drawing.Point(213, 12);
+            this.drawingLines.Name = "drawingLines";
+            this.drawingLines.Size = new System.Drawing.Size(120, 84);
+            this.drawingLines.TabIndex = 3;
+            this.drawingLines.SelectedIndexChanged += new System.EventHandler(this.drawingLines_SelectedIndexChanged);
+            // 
+            // toolState
+            // 
+            this.toolState.AutoSize = true;
+            this.toolState.Location = new System.Drawing.Point(1035, 31);
+            this.toolState.Name = "toolState";
+            this.toolState.Size = new System.Drawing.Size(121, 17);
+            this.toolState.TabIndex = 2;
+            this.toolState.Text = "Tool Being Used: ";
+            // 
+            // drawLineButton
+            // 
+            this.drawLineButton.Location = new System.Drawing.Point(39, 41);
+            this.drawLineButton.Name = "drawLineButton";
+            this.drawLineButton.Size = new System.Drawing.Size(168, 23);
+            this.drawLineButton.TabIndex = 1;
+            this.drawLineButton.Text = "Draw Line";
+            this.drawLineButton.UseVisualStyleBackColor = true;
+            this.drawLineButton.Click += new System.EventHandler(this.roadButton_Click);
+            // 
+            // viewButton
+            // 
+            this.viewButton.Location = new System.Drawing.Point(39, 12);
+            this.viewButton.Name = "viewButton";
+            this.viewButton.Size = new System.Drawing.Size(168, 23);
+            this.viewButton.TabIndex = 0;
+            this.viewButton.Text = "View";
+            this.viewButton.UseVisualStyleBackColor = true;
+            this.viewButton.Click += new System.EventHandler(this.paneButton_Click);
+            // 
+            // canvasPanel
+            // 
+            this.canvasPanel.BackColor = System.Drawing.Color.White;
+            this.canvasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvasPanel.Location = new System.Drawing.Point(0, 125);
+            this.canvasPanel.Name = "canvasPanel";
+            this.canvasPanel.Size = new System.Drawing.Size(1272, 523);
+            this.canvasPanel.TabIndex = 1;
+            this.canvasPanel.Click += new System.EventHandler(this.canvasPanel_Click);
+            this.canvasPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.canvasPanel_Paint);
+            this.canvasPanel.MouseHover += new System.EventHandler(this.canvasPanel_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1272, 648);
+            this.Controls.Add(this.canvasPanel);
+            this.Controls.Add(this.topPanel);
+            this.Name = "Form1";
+            this.Text = "Fictional Mapper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.topPanel.ResumeLayout(false);
+            this.topPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Panel canvasPanel;
+        private System.Windows.Forms.Button drawLineButton;
+        private System.Windows.Forms.Button viewButton;
+        private System.Windows.Forms.Label toolState;
+        private System.Windows.Forms.ListBox drawingLines;
+        private System.Windows.Forms.ListBox drawingRegions;
+        private System.Windows.Forms.Button drawRegionButton;
+        private System.Windows.Forms.ListBox shieldType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox shieldBoxToggle;
+        private System.Windows.Forms.TextBox routeNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox selectionNameBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FictionalMapper
+{
+    public partial class Form1 : Form
+    {
+        public LineType currentLineType;
+        public RegionType currentRegionType;
+        public Pen lineMain;
+        public Pen lineOutline;
+        public Pen drawing;
+        public Brush region;
+        public Graphics g;
+        public Tool tool;
+        public List<Point> points;
+        public ArrayList elements;
+        public string selectionName;
+        public Func<Point, Point, int> distance = (Point p1, Point p2) =>
+        {
+            return (int)Math.Sqrt(Math.Pow(p2.Y - p1.Y, 2.0) + Math.Pow(p2.X - p1.X, 2.0));
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            g = canvasPanel.CreateGraphics();
+            tool = Tool.VIEW;
+            toolState.Text = "Tool Being Used: View";
+            lineMain = new Pen(Colors.highwayInterior, 5);
+            lineOutline = new Pen(Colors.highwayExterior, 7);
+            drawing = new Pen(Color.Black, 5);
+            points = new List<Point>();
+            region = new SolidBrush(Colors.water);
+            elements = new ArrayList();
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void roadButton_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+
+            tool = Tool.LINE;
+            toolState.Text = "Tool Being Used: Line";
+        }
+
+        private void paneButton_Click(object sender, EventArgs e) // viewButton
+        {
+            g.Clear(Color.White);
+            selectionName = selectionNameBox.Text;
+            switch (tool)
+            {
+                case Tool.LINE:
+                    Line l = new Line();
+                    l.roadName = selectionName;
+                    l.points = points;
+                    l.lineType = currentLineType;
+                    elements.Add(l);
+                    break;
+                case Tool.REGION:
+                    Region r = new Region();
+                    r.points = points;
+                    r.regionType = currentRegionType;
+                    elements.Add(r);
+                    break;
+            }
+            
+            foreach(object o in elements)
+            {
+                int viewableDistance = 0;
+                if (o.GetType() == Type.GetType("FictionalMapper.Line", false, false))
+                {
+                    switch (((Line)o).lineType)
+                    {
+                        case LineType.HIGHWAY:
+                            lineOutline.Color = Colors.highwayExterior;
+                            lineMain.Color = Colors.highwayInterior;
+                            break;
+                        case LineType.ROAD:
+                            lineOutline.Color = Colors.roadExterior;
+                            lineMain.Color = Colors.roadInterior;
+                            break;
+                        case LineType.CREEK:
+                            lineMain.Color = Colors.water;
+                            break;
+                    }
+                    
+                    for (int i = 1; i < ((Line)o).points.Count; i++)
+                    {
+                        if(!(((Line)o).lineType == LineType.CREEK))
+                        {
+                            g.DrawLine(lineOutline, ((Line)o).points[i - 1], ((Line)o).points[i]);
+                        }
+                        viewableDistance += distance.Invoke(((Line)o).points[i - 1], ((Line)o).points[i]);
+                        g.DrawLine(lineMain, ((Line)o).points[i - 1], ((Line)o).points[i]);
+                    }
+                } else if(o.GetType() == Type.GetType("FictionalMapper.Region", false, false))
+                {
+                    g.FillPolygon(region, ((Region)o).points.ToArray());
+                }
+                //MessageBox.Show("viewableDistance: " + viewableDistance.ToString());
+                if (viewableDistance > selectionName.Length * 5)
+                {
+                    //MessageBox.Show(((Line)o).roadName);
+                    Label label = new Label();
+                    label.Text = ((Line)o).roadName;
+                    label.Location = ((Line)o).points[1];
+                    label.Font = new Font("Calibri", 18);
+                    label.ForeColor = Color.Black;
+                    label.Padding = new Padding(6);
+                    label.Visible = true;
+                    Controls.Add(label);
+                    //write road in between text
+                }
+            }
+            tool = Tool.VIEW;
+            toolState.Text = "Tool Being Used: View";
+        }
+
+        private void canvasPanel_MouseHover(object sender, EventArgs e)
+        {
+            switch (tool)
+            {
+                case Tool.VIEW:
+                    Cursor.Current = Cursors.Hand;
+                    break;
+                case Tool.LINE:
+                case Tool.REGION:
+                    Cursor.Current = Cursors.Cross;
+                    break;
+            }
+        }
+
+        private void canvasPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void canvasPanel_Click(object sender, EventArgs e)
+        {
+            Point p = canvasPanel.PointToClient(Cursor.Position);
+            g.DrawEllipse(new Pen(Color.Black, 1), new Rectangle(new Point(p.X-3, p.Y-3), new Size(6, 6)));
+            points.Add(p);
+        }
+
+        private void drawTools_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void drawRegionButton_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+
+            tool = Tool.REGION;
+            toolState.Text = "Tool Being Used: Region";
+        }
+
+        private void drawingLines_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (drawingLines.SelectedItem.ToString())
+            {
+                case "Highway":
+                    currentLineType = LineType.HIGHWAY;
+                    break;
+                case "Road":
+                    currentLineType = LineType.ROAD;
+                    break;
+                case "Creek":
+                    currentLineType = LineType.CREEK;
+                    break;
+            }
+        }
+
+        private void drawingRegions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (drawingRegions.SelectedItem.ToString())
+            {
+                case "Urban":
+                    currentRegionType = RegionType.URBAN_LAND;
+                    break;
+                case "Rural":
+                    currentRegionType = RegionType.RURAL_LAND;
+                    break;
+                case "Nature":
+                    currentRegionType = RegionType.NATURE_LAND;
+                    break;
+                case "Beach":
+                    currentRegionType = RegionType.BEACH;
+                    break;
+                case "Water":
+                    currentRegionType = RegionType.WATER;
+                    break;
+            }
+        }
+
+        private void shieldBosToggle_CheckedChanged(object sender, EventArgs e)
+        {
+            shieldType.Enabled = shieldBoxToggle.Checked;
+            routeNumber.Enabled = shieldBoxToggle.Checked;
+        }
+        private void routeNumber_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void routeNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+    public enum Tool
+    {
+        VIEW = 0,
+        LINE = 1,
+        REGION = 2
+    }
+    public enum LineType
+    {
+        HIGHWAY = 0,
+        ROAD = 1,
+        CREEK = 2
+    }
+    public enum RegionType
+    {
+        URBAN_LAND = 0,
+        RURAL_LAND = 1,
+        NATURE_LAND = 2,
+        BEACH = 3, 
+        WATER = 4
+    }
+    public class Line
+    {
+        public int highwayNum { get; set; }
+        public string roadName { get; set; }
+        public List<Point> points { get; set; }
+        public LineType lineType { get; set; }
+        public override string ToString()
+        {
+            return "lineType: " + this.lineType.ToString();
+        }
+    }
+
+    public class Region
+    {
+        public List<Point> points { get; set; }
+        public RegionType regionType { get; set; }
+    }
+    public static class Colors
+    {
+        // Roads
+        public static Color highwayInterior = Color.FromArgb(253, 226, 147);
+        public static Color highwayExterior = Color.FromArgb(252, 179, 23);
+        public static Color roadInterior = Color.White;
+        public static Color roadExterior = Color.FromArgb(221, 221, 228);
+        // Land
+        public static Color urbanLand = Color.FromArgb(232, 234, 237);
+        public static Color ruralLand = Color.FromArgb(187, 226, 198);
+        public static Color natureLand = Color.FromArgb(168, 218, 181);
+        public static Color beach = Color.FromArgb(255, 236, 196);
+        // Water
+        public static Color water = Color.FromArgb(160, 196, 252);
+    }
+}
